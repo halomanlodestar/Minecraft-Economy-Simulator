56bf3d3b1749c238a382ef2129d06cc6a006c0ae
halomanlodestar <kunalranarj2005@gmail.com>
2023-06-03 19:23:59 +0530

    Added a discord webhook

--- dist/backend/index.js
+++ dist/backend/index.js
@@ -2,8 +2,6 @@
 /** @format */
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("./mongo/mongoose");
-// import transaction from "./Transaction";
-//
 async function run() {
     (0, mongoose_1.connectDatabase)();
 }
--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,9 +1,6 @@
 /** @format */
 
 import { connectDatabase } from "./mongo/mongoose";
-// import transaction from "./Transaction";
-
-//
 async function run() {
 	connectDatabase();
 }
